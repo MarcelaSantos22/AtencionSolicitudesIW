a70d7f237a6d0d3b62aaacb394fc8563104fa80a
MarcelaSantos22 <ymarcela.quejada@udea.edu.co>
2017-04-26 22:20:50 -0500

    Inicio del proyecto final, creacion DTO y Excepcion

---
+++ README.md
@@ -0,0 +1,1 @@
+"Proyecto final del curso Ing. Web. Modulo Atencion de Solicitudes" 
---
+++ src/dto/Sucursal.java
@@ -0,0 +1,51 @@
+package dto;
+
+/**
+ * DTO Clase para manejar la tabla Sucursal de la Base de Datos
+ * atencionSolicitudes.
+ * 
+ * @author Yuri Quejada
+ * @author Daniel Pelaez
+ * @author Jean Herrera
+ * @version 1.0
+ */
+
+
+public class Sucursal {
+	private int id;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+}
